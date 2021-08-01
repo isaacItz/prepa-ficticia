3956b642d0f8e83b27a303e03c334975f8888621
Isaac Lugo <isaac.est.cbtis@gmail.com>
2021-07-31 23:46:29 -0500

    vista

--- src/vista/FormularioPersona.java
+++ src/vista/FormularioPersona.java
@@ -1,56 +1,115 @@
 package vista;
 
-import javax.swing.JPanel;
 import java.awt.GridLayout;
+
+import javax.swing.ButtonGroup;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+import com.toedter.calendar.JDateChooser;
 
 public class FormularioPersona extends JPanel {
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField cajaNombre;
+	private JTextField cajaPaterno;
+	private JTextField cajaMaterno;
+	private JTextField cajaCurp;
+	private JRadioButton radioMasculino;
+	private JRadioButton radioFemenino;
+	private JDateChooser dateChooser;
 
 	/**
 	 * Create the panel.
 	 */
 	public FormularioPersona() {
-		setLayout(new GridLayout(0, 1, 0, 10));
-		
-		JLabel lblNombre = new JLabel("Nombre:");
-		add(lblNombre);
-		
-		textField = new JTextField();
-		add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblApellidoPaterno = new JLabel("Apellido Paterno:");
-		add(lblApellidoPaterno);
-		
-		textField_1 = new JTextField();
-		add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblAppellidoMaterno = new JLabel("Appellido Materno:");
-		add(lblAppellidoMaterno);
-		
-		textField_2 = new JTextField();
-		add(textField_2);
-		textField_2.setColumns(10);
-		
-		JLabel lblSexo = new JLabel("Sexo:");
-		add(lblSexo);
-		
-		JPanel panel = new JPanel();
-		add(panel);
-		panel.setLayout(new GridLayout(0, 2, 0, 0));
-		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Masculino");
-		panel.add(rdbtnNewRadioButton);
-		
-		JRadioButton rdbtnFemenino = new JRadioButton("Femenino");
-		panel.add(rdbtnFemenino);
+		ButtonGroup grupo = new ButtonGroup();
+		setLayout(new GridLayout(1, 0, 1, 10));
 
+		JPanel panelPersonales = new JPanel();
+		panelPersonales.setBorder(
+				new TitledBorder(null, "Datos Personales", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		add(panelPersonales);
+		panelPersonales.setLayout(new GridLayout(0, 1, 0, 10));
+
+		JLabel lblNombre = new JLabel("Nombre:");
+		panelPersonales.add(lblNombre);
+
+		cajaNombre = new JTextField();
+		panelPersonales.add(cajaNombre);
+		cajaNombre.setColumns(10);
+
+		JLabel lblApellidoPaterno = new JLabel("Apellido Paterno:");
+		panelPersonales.add(lblApellidoPaterno);
+
+		cajaPaterno = new JTextField();
+		panelPersonales.add(cajaPaterno);
+		cajaPaterno.setColumns(10);
+
+		JLabel lblAppellidoMaterno = new JLabel("Appellido Materno:");
+		panelPersonales.add(lblAppellidoMaterno);
+
+		cajaMaterno = new JTextField();
+		panelPersonales.add(cajaMaterno);
+		cajaMaterno.setColumns(10);
+
+		JLabel lblSexo = new JLabel("Sexo:");
+		panelPersonales.add(lblSexo);
+
+		JPanel panel = new JPanel();
+		panelPersonales.add(panel);
+		panel.setLayout(new GridLayout(0, 2, 0, 0));
+
+		radioMasculino = new JRadioButton("Masculino");
+		panel.add(radioMasculino);
+
+		radioFemenino = new JRadioButton("Femenino");
+		panel.add(radioFemenino);
+
+		JLabel lblNewLabel = new JLabel("Fecha de Nacimiento:");
+		panelPersonales.add(lblNewLabel);
+
+		dateChooser = new JDateChooser();
+		panelPersonales.add(dateChooser);
+
+		JLabel lblCurp = new JLabel("CURP:");
+		panelPersonales.add(lblCurp);
+
+		cajaCurp = new JTextField();
+		panelPersonales.add(cajaCurp);
+		cajaCurp.setColumns(10);
+		grupo.add(radioFemenino);
+		grupo.add(radioMasculino);
+
+	}
+
+	public JTextField getCajaNombre() {
+		return cajaNombre;
+	}
+
+	public JTextField getCajaPaterno() {
+		return cajaPaterno;
+	}
+
+	public JTextField getCajaMaterno() {
+		return cajaMaterno;
+	}
+
+	public JTextField getCajaCurp() {
+		return cajaCurp;
+	}
+
+	public JRadioButton getRadioMasculino() {
+		return radioMasculino;
+	}
+
+	public JRadioButton getRadioFemenino() {
+		return radioFemenino;
+	}
+
+	public JDateChooser getDateChooser() {
+		return dateChooser;
 	}
 
 }
