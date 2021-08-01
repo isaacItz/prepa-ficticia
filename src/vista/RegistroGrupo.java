76453d4b106b0b3329b5eaccdaa0d38ced158f96
Isaac Lugo <isaac.est.cbtis@gmail.com>
2021-08-01 17:31:46 -0500

    calificaciones ya casi esta bron!

--- src/vista/RegistroGrupo.java
+++ src/vista/RegistroGrupo.java
@@ -3,50 +3,77 @@ package vista;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
+import modelo.Docente;
+import modelo.Docentes;
 import modelo.Grupos;
 
 public class RegistroGrupo extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textField;
-	private JTextField textField_1;
+	private JComboBox textField;
+	private JComboBox textField_1;
 	private Grupos grupos;
-
+	private JComboBox textField_2;
+	private DefaultComboBoxModel<String> modelDocentes;
+	private Docentes docentes;
 
 	public RegistroGrupo(Grupos grupos) {
+		modelDocentes = new DefaultComboBoxModel<String>();
 		this.grupos = grupos;
-		
+
 		setModal(true);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(new GridLayout(0, 1, 30, 30));
+		contentPanel.setLayout(new GridLayout(0, 1, 30, 10));
 		{
 			JLabel lblGrado = new JLabel("Grado:");
 			contentPanel.add(lblGrado);
 		}
 		{
-			textField = new JTextField();
+			textField = new JComboBox();
+			textField.setModel(new DefaultComboBoxModel(
+					new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" }));
 			contentPanel.add(textField);
-			textField.setColumns(10);
 		}
 		{
 			JLabel lblLetra = new JLabel("Letra:");
 			contentPanel.add(lblLetra);
 		}
 		{
-			textField_1 = new JTextField();
+			textField_1 = new JComboBox();
+			textField_1.setModel(new DefaultComboBoxModel(new String[] { "A", "B", "C", "D", "E", "F" }));
 			contentPanel.add(textField_1);
-			textField_1.setColumns(10);
+		}
+		{
+			JLabel lblDocente = new JLabel("Docente:");
+			contentPanel.add(lblDocente);
+		}
+		{
+			textField_2 = new JComboBox();
+			contentPanel.add(textField_2);
+		}
+		{
+			JLabel lblCantidadDeParciales = new JLabel("Cantidad de Parciales:");
+			contentPanel.add(lblCantidadDeParciales);
+		}
+		{
+			JComboBox comboBox = new JComboBox();
+			comboBox.setModel(new DefaultComboBoxModel(
+					new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15" }));
+			contentPanel.add(comboBox);
 		}
 		{
 			JPanel buttonPane = new JPanel();
@@ -60,9 +87,25 @@ public class RegistroGrupo extends JDialog {
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						dispose();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
+		}
+		llenarDocentes();
+	}
+
+	public void setDocentes(Docentes docentes) {
+		this.docentes = docentes;
+	}
+
+	private void llenarDocentes() {
+		for (Docente d : docentes.getDocentes()) {
+			modelDocentes.addElement(d.getNombreCompleto());
 		}
 	}
 
